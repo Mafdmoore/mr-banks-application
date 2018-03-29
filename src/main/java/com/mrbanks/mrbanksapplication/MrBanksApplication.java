703deeb989790140bba0fbcda4db89e9af0fc1d3
Mafdmoore <mafdmoore@gmail.com>
2018-03-29 14:33:05 +0100

    Initial project commit.

---
+++ src/main/java/com/mrbanks/mrbanksapplication/MrBanksApplication.java
@@ -0,0 +1,13 @@
+package com.mrbanks.mrbanksapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MrBanksApplication
+{
+	public static void main(String[] args)
+	{
+		SpringApplication.run(MrBanksApplication.class, args);
+	}
+}
