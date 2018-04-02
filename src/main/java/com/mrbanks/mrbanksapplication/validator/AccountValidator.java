026ff9aab8d2499e4d378c1545ff9b5cb0c4e07f
Matthew Moore <mafdmoore@gmail.com>
2018-04-02 17:14:20 +0100

    Working alpha version.

--- src/main/java/com/mrbanks/mrbanksapplication/validator/AccountValidator.java
+++ src/main/java/com/mrbanks/mrbanksapplication/validator/AccountValidator.java
@@ -1,19 +1,14 @@
 package com.mrbanks.mrbanksapplication.validator;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
-import com.mrbanks.mrbanksapplication.dao.AccountDAO;
 import com.mrbanks.mrbanksapplication.dao.AccountForm;
  
 @Component
 public class AccountValidator implements Validator
 {
-    @Autowired
-    private AccountDAO appUserDAO;
- 
     @Override
     public boolean supports(Class<?> clazz)
     {
@@ -23,20 +18,8 @@ public class AccountValidator implements Validator
     @Override
     public void validate(Object target, Errors errors)
     {
-    	AccountForm accountForm = (AccountForm) target;
- 
-        //Check the fields of AccountForm.
-        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "firstName", "NotEmpty.appUserForm.firstName");
-        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "lastName", "NotEmpty.appUserForm.lastName");
- 
-        if (accountForm.getAccountId() == null)
-        {
-            //Account dbAccount = appUserDAO.findAccountByAccountId(accountForm.getEmail());
-            //if (dbAccount != null)
-            //{
-                //Email has been used by another account.
-            //    errors.rejectValue("email", "Duplicate.appUserForm.email");
-            //}
-        }
+    	//Check the fields of AccountForm object
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "firstName", "NotEmpty.accountForm.firstName");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "lastName", "NotEmpty.accountForm.lastName");
     }
 }
